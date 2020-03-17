2960276c75de11224d1f6be63291eb3f9147532b
ilkrbk <ilkrbk@gmail.com>
2020-03-17 11:44:05 +0200

    final commit

--- SelectionSort/Program.cs
+++ SelectionSort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SelectionSort
 {
@@ -6,7 +7,26 @@ namespace SelectionSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> array = new List<int>() {9, 2, 4, 5, 3, 1, 8, 6, 7};
+            for (int i = 0; i < array.Count; i++)
+            {
+                int min = i;
+                for (int j = i; j < array.Count; j++)
+                {
+                    if (array[j] < array[min])
+                    {
+                        min = j;
+                    }
+                }
+                int temp = array[min];
+                array[min] = array[i];
+                array[i] = temp;
+            }
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
         }
     }
 }
